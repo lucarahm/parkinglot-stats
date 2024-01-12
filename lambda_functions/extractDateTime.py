84a996ed9ff1e9b265b4406e185207147cf9a663
Simon Draxl <simon.n.draxl@student.uibk.ac.at>
2024-01-12 22:13:03 +0100

    add extractDateTime code

--- lambda_functions/extractDateTime.py
+++ lambda_functions/extractDateTime.py
@@ -1,9 +1,22 @@
 import json
-
+from datetime import datetime
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    # use in production environment
+    # image_name = event['Image']['S3Object']['Name']
+    
+    image_name = '2012-09-12_06_36_36_jpg.rf.08869047c7e9f62f5ce9334546b52958.jpg'
+    
+    # Extracting date and time components
+    date_str = image_name[:10]
+    time_str = image_name[11:19].replace('_', ':')
+    
+    # 0 for monday
+    weekday =  datetime.strptime(date_str, '%Y-%m-%d').date().weekday()
+    
     return {
-        "date": "02.01.2010",
-        "time": "12"
+        "date": date_str,
+        "time": time_str,
+        "weekday": weekday
     }
