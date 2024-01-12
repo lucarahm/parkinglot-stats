f2db87c3ea4763c28b62ad9150c2cdb16265ee95
Simon Draxl <simon.n.draxl@student.uibk.ac.at>
2024-01-12 21:51:00 +0100

    add rekognition code

--- lambda_functions/countEmptySpots.py
+++ lambda_functions/countEmptySpots.py
@@ -1,9 +1,45 @@
 import json
+import boto3
 
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    rekognition = boto3.client('rekognition', 
+        region_name='us-east-1')
+    
+    # used to simulate event input 
+    #event = {
+    #    'Image':{
+    #        'S3Object': {
+    #            'Bucket': 'dsprojecttestbucket',
+    #            'Name': '2012-09-12_06_36_36_jpg.rf.08869047c7e9f62f5ce9334546b52958.jpg'
+    #        }
+    #    }
+    #}
+    
+    s3_bucket = event['Image']['S3Object']['Bucket']
+    image_name = event['Image']['S3Object']['Name']
+    
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': s3_bucket,
+                'Name': image_name
+            }
+        }
+    )
+    
+    target_labels = ['Car', 'Motorcycle']
+    instances_count = 0
+    for target_label in target_labels:
+        for label in response['Labels']:
+            if label['Name'] == target_label:
+                instances_count += len(label.get('Instances', []))
+
+    # Print the instances count
+    print(f"The labels '{target_labels}' appears {instances_count} times in the image.")
+    
     return {
-        'total': 300,
-        'empty': 122
+        'total': 47,
+        'taken': instances_count
     }
