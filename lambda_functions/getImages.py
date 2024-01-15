a096790e23fa557fd55277106d66f3734779b104
Simon Draxl <simon.n.draxl@student.uibk.ac.at>
2024-01-15 20:51:42 +0100

    add getImages code

--- lambda_functions/getImages.py
+++ lambda_functions/getImages.py
@@ -1,16 +1,25 @@
 import json
-
+import boto3
 
 def lambda_handler(event, context):
     # Input should be S3 Bucket
-
-    # Returns Array of all the images in the folder
-
-    return [{
-        "Image": {
-            "S3Object": {
-                "Bucket": "rekognition-console-v4-prod-iad",
-                "Name": "assets/StaticImageAssets/SampleImages/skateboard.jpg"
+    
+    s3 = boto3.client('s3', region_name='us-east-1')
+    bucket_name = 'dsprojecttestbucket'
+    response = s3.list_objects(Bucket=bucket_name)
+    image_keys = [obj['Key'] for obj in response.get('Contents', [])]
+    
+    return_json = []
+    for key in image_keys:
+        image  = {
+            "Image": {
+                "S3Object": {
+                    "Bucket": bucket_name,
+                    "Name": key
+                }
             }
         }
-    }]
+        return_json.append(image)
+
+    # Returns Array of all the images in the folder
+    return return_json
