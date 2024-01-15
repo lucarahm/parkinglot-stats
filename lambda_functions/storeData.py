443a1fcea6d5e8fff4d5b67678775a10b6182231
Simon Draxl <simon.n.draxl@student.uibk.ac.at>
2024-01-15 20:50:28 +0100

    add storeData code

--- lambda_functions/storeData.py
+++ lambda_functions/storeData.py
@@ -1,33 +1,32 @@
 import json
-
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
-    return [{
-        'databaseURI': "redis",
-        'weekday': "Monday"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "Tuesday"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "Wed"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "Thu"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "Fri"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "Sat"
-    },
-    {
-        'databaseURI': "redis",
-        'weekday': "S"
-    }]
+    
+    table_name = 'parking_data'
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+        
+    for e in event:
+        key = e['date'] + '_' + e['time']
+        try:
+            response = table.put_item(
+                Item={
+                'fan': key,
+                'value': e
+            }
+        )
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': f'Error: {str(e)}'
+            }
+       
+    return_json = []     
+    for i in range(7):
+        return_json.append({
+        'table': table_name,
+        'weekday': i
+    })
+    
+    return return_json
