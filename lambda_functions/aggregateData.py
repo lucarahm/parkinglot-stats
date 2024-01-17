69c957ca59624c893c28c7b8bb2acf9e4712f63d
alexander <alexander.montag@student.uibk.ac.at>
2024-01-17 12:33:40 +0100

    update aggregateData to aggregate in python code

--- lambda_functions/aggregateData.py
+++ lambda_functions/aggregateData.py
@@ -12,23 +12,34 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     dbtable = dynamodb.Table(table)
 
-    aggregatedData = []
+    aggregated_data = {}
+    count_data = {}
 
     items = dbtable.scan()['Items']
     for item in items:
-        itemValue = item['value']
-        if (itemValue['Weekday'] != weekday):
+        item_value = item['value']
+        if (item_value['Weekday'] != weekday):
             continue
-        aggregation = {
-            'Time': itemValue['Time'],
-            'Taken': itemValue['Taken']
-        }
-        aggregatedData.append(aggregation)
+
+        time_value = item_value.get('Time')
+        taken_value = item_value.get('Taken')
+
+        if time_value in aggregated_data:
+            aggregated_data[time_value]['Taken'] += taken_value
+            count_data[time_value] += 1
+        else:
+            aggregated_data[time_value] = {'Time': time_value, 'Taken': taken_value}
+            count_data[time_value] = 1
+
+    for time_value, data in aggregated_data.items():
+        data['Taken'] /= count_data[time_value]
+
+    aggregated_data_list = list(aggregated_data.values())
 
     return {
         'S3Bucket': s3_bucket,
         'PlotPath': plot_path,
         'TotalSpots': total_spots,
         'Weekday': weekday,
-        'Data': aggregatedData
+        'Data': aggregated_data_list
     }
